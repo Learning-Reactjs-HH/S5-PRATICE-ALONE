636b843fc8f64958979f423a964bda0796fb6bce
Hoàng <hoanghung20030@gmail.com>
2024-05-16 12:01:57 +0700

    finish exercise 5

--- Start code/exercise-5/src/App.jsx
+++ Start code/exercise-5/src/App.jsx
@@ -1,5 +1,21 @@
+import Card from "./components/Card";
+import { AVAILABLE_DATA } from "./data";
 function App() {
-  return <>{/* Your code  here */}</>;
+  return (
+    <>
+      <header>
+        <h1>My Items</h1>
+      </header>
+
+      <div class="cards-view">
+        <div class="cards-grid">
+          {AVAILABLE_DATA.map((data)=>(
+            <Card card={data} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- Start code/exercise-5/src/data.js
+++ Start code/exercise-5/src/data.js
@@ -1,1 +1,62 @@
-/* Your data here */
+export const AVAILABLE_DATA = [
+    {
+        id:'id1',
+        name:'Winter is Coming',
+        title:'Whether',
+        description:'The Winter is coming, the whether is very cool. But I very like it.',
+        image:{
+            src:'https://i.pinimg.com/originals/fd/45/57/fd4557983a806ffe60508fb43c4a3b91.jpg',
+            alt:'avatar'
+        }
+    },
+    {
+        id:'id2',
+        name:'Summer is Coming',
+        title:'Wheter',
+        description:'The Summer is comming so I will go to the beach and go swim.',
+        image:{
+            src:'https://i.pinimg.com/236x/ff/74/34/ff7434d62b6eb6c8d32465bf917ac59b.jpg',
+            alt:'avatar'
+        }
+    },
+    {
+        id:'id3',
+        name:'Lunar  New Year',
+        title:'Tet',
+        description:'Everyone will comeback to their house and gather with their families',
+        image:{
+            src:'https://i.pinimg.com/originals/e5/4e/7c/e54e7c64903117cd799fd6d045b43fd6.jpg',
+            alt:'avatar'
+        }
+    },
+    {
+        id:'id4',
+        name:'Activity',
+        title:'School',
+        description:'Football is a type of sports, it very good for health',
+        image:{
+            src:'https://instructionalseries.tki.org.nz/var/tki-eesoll/storage/images/instructional-series/school-journal/school-journal-level-2-august-2018/football-the-beautiful-game/459993-1-eng-NZ/Football-The-Beautiful-Game_resource_image.png',
+            alt:'avatar'
+        }
+    },
+    {
+        id:'id5',
+        name:'Class work',
+        title:'Class B',
+        description:'Learning new things',
+        image:{
+            src:'https://thumbs.dreamstime.com/b/girl-doing-homework-explore-diligent-world-education-girl-doing-homework-scholarly-scene-captures-essence-299778689.jpg',
+            alt:'avatar'
+        }
+    },
+    {
+        id:'id6',
+        name:'Winter',
+        title:'Whether',
+        description:'Whether is cold',
+        image:{
+            src:'https://i.pinimg.com/236x/e8/74/1c/e8741c02b30a1681978c0a5c4154db5c.jpg',
+            alt:'avatar'
+        }
+    },
+];
